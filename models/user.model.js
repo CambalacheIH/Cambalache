e25d77e6271af690c23206ab5c2ebc9660e8e1e1
Pablo Laso <plasocortabitarte@gmail.com>
2018-02-06 21:10:08 +0100

    First user model fixes #3

--- models/user.model.js
+++ models/user.model.js
@@ -3,11 +3,6 @@ const bcrypt = require('bcrypt');
 const SALT_WORK_FACTOR = 10;
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, 'Username is required'],
-    unique: true
-  },
   password: {
     type: String,
     required: [true, 'Password is required']
@@ -19,7 +14,35 @@ const userSchema = new mongoose.Schema({
     match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
     unique: true
   },
-  city: {
-    
+  name: {
+    type: String,
+    required: [true, 'A name is required']
+  },
+  surname: {
+    type: String
   }
+}, {timestamps: true});
+
+userSchema.pre('save', function (next) {
+  const user = this;
+  if (!user.isModified('password')) {
+      return next();
+  }
+
+  bcrypt.genSalt(SALT_WORK_FACTOR)
+      .then(salt => {
+          bcrypt.hash(user.password, salt)
+              .then(hash => {
+                  user.password = hash;
+                  next();
+              })
+      })
+      .catch(error => next(error));
 });
+
+userSchema.methods.checkPassword = function (password) {
+  return bcrypt.compare(password, this.password);
+}
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
