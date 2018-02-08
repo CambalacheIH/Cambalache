5637dda642e899a552e634fae5363a6d4b57b721
Pablo Laso <plasocortabitarte@gmail.com>
2018-02-08 19:11:36 +0100

    feat: local-auth login passport in controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -26,3 +26,38 @@ module.exports.doSignup = (req, res, next) => {
           }  
       }).catch(error => next(error));
 }
+
+module.exports.login = (req, res, next) => {
+    res.render('auth/login');
+}
+
+module.exports.doLogin = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (!username || !password) {
+        res.render('auth/login', {
+            user: { email: email },
+            error: {
+                email: email ? '' : 'Email is required',
+                password: password ? '' : 'Password is required'
+            }
+        });
+    } else {
+        passport.authenticate('local-auth', (error, user, validation) => {
+            if (error) {
+                next(error);
+            } else if (!user) {
+                res.render('auth/login', {error: validation});
+            } else {
+                req.login(user, (error) => {
+                    if (error) {
+                        next(error);
+                    } else {
+                        res.redirect('/');
+                    }
+                });
+            }
+        })(req, res, next);
+    }
+}
