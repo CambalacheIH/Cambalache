3c95c75d7c74c2dde3c9e81a7b78d1b2747c6496
proyola <palomaoyola@gmail.com>
2018-02-24 17:52:51 +0100

    fixes mongo uri local

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 const DB_NAME = 'cambalache';
 const MONGODB_URI = process.env.MONGODB_URI;
-console.log(MONGODB_URI);
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI+DB_NAME)
     .then(() => {
-        console.info(`Connect to db ${MONGODB_URI+DB_NAME}`);
+        console.info(`Connect to db ${DB_NAME}`);
     })
     .catch(error => {
         console.error(`Unable to connect to db ${DB_NAME}: ${error}`);
